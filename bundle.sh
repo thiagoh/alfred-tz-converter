b45176e8a2f7278e7a1a8f102101026044f88137
Thiago Andrade <thiagsou@amazon.com>
2024-01-03 20:06:35 -0500

    Fix Bundler to avoid increasing bundle indefinetely

--- bundle.sh
+++ bundle.sh
@@ -3,6 +3,8 @@
 rm -rf /tmp/alfred-tz-converter
 mkdir /tmp/alfred-tz-converter
 cp -r * /tmp/alfred-tz-converter
+
+rm -rf /tmp/alfred-tz-converter/*.alfredworkflow
 rm -rf /tmp/alfred-tz-converter/.git
 
-f=alfred-tz-converter.alfredworkflow && rm $f ; ditto -ck /tmp/alfred-tz-converter $f && open $f
+f=alfred-tz-converter.alfredworkflow && ditto -ck /tmp/alfred-tz-converter $f && open $f
